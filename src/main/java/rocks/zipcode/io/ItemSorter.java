ed092bf001c324124bdb0f7670d3e3232d4b9721
James <james@Zipcoders-MacBook-Pro-101.local>
2023-04-03 13:36:07 -0400

    finished comparators

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,24 @@
 package rocks.zipcode.io;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+
+    Item[] itemArray;
+
+
     public ItemSorter(Item[] items) {
+        itemArray = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        Arrays.sort(itemArray);
+        return itemArray;
     }
 }
